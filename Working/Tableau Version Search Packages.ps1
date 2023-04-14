ae617c204cfc43c12f2747d9fa9a78bbe0e220b8
Michael Surtees <msurtees@me.com>
2023-04-14 06:15:31 -0400

    Awaiting Client's Uninstall Preferences

--- Working/Tableau Version Search Packages.ps1
+++ Working/Tableau Version Search Packages.ps1
@@ -2,13 +2,13 @@
 $0353Installed = "no"
 $versionCheck = Get-Package -Name '*Tableau*' | Where-Object -Property ProviderName -ne 'msi' | Sort-Object -Property Name | Select-Object -Property Name 
 $versionCheck.Name  | Out-File "c:\temp\versionCheck.csv"
-if (Select-String -Path C:\temp\versionCheck.csv -Pattern ".0353" -SimpleMatch)
+if (Select-String -Path 'C:\temp\versionCheck.csv' -Pattern ".0353" -SimpleMatch)
     {echo "Latest version installed"
     $0353Installed = "yes"}
 else
     {Start-Process "C:\temp\TableauDesktop-64bit-2022-4-2.exe" -ArgumentList "ACCEPTEULA=1 DESKTOPSHORTCUT=1 REMOVEINSTALLEDAPP=1 /quiet" -PassThru
 }
-
+$versionArray = import-csv "c:\temp\versionCheck.csv"
 
 if ($0353Installed -eq "yes")
 {
@@ -20,14 +20,5 @@ else{echo "Old version 1841 not installed"}
 else{echo "Old version 1653 not installed"}
 }
 
-$a= Get-Package -Name "*
-| fl
 
-#>
-if ($findString -eq "*.0353") {write-host "yes"} else {write-host "no"}
-$findString = select-string  -path  "C:\temp\versionCheck.csv" -pattern "0353"
-$findString = select-string  -path  "C:\temp\versionCheck.csv" -pattern "0353"
-$findString = select-string  -path  "C:\temp\versionCheck.csv" -pattern "0353"
-$findString = select-string  -path  "C:\temp\versionCheck.csv" -pattern "0353"
-
-import into array?
+uninstall-package
